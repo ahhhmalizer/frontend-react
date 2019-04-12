434893063a3f43bdfc843543595d7af43645f792
Daniel Habenicht <daniel-habenicht@outlook.de>
2019-04-13 01:11:07 +0200

    WIP Upload

--- my-app/src/pages/upload-video/index.tsx
+++ my-app/src/pages/upload-video/index.tsx
@@ -1,9 +1,26 @@
 import * as React from 'react';
 
-export function UploadVideo() {
-  return (
-    <div>
-      upload video
-    </div>
-  )
+export class UploadVideo extends React.Component<{}, {}> {
+  public blobService = null;
+  constructor() {
+    super({});
+    const blobUri = 'https://' + 'STORAGE_ACCOUNT' + '.blob.core.windows.net';
+    this.blobService = (window as any).AzureStorage.Blob.createBlobServiceWithSas(blobUri, 'SAS_TOKEN');
+  }
+
+  public render() {
+    return <div>upload video</div>;
+  }
+
+  public uploadfile() {
+    if (this.blobService != null) {
+      this.blobService.createContainerIfNotExists('mycontainer', function(error, result) {
+        if (error) {
+          // Create container error
+        } else {
+          // Create container successfully
+        }
+      });
+    }
+  }
 }
