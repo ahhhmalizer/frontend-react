9ae658fc26e7c847123c2386ee4d8d097d4298dc
Edgar Dorausch <EdgarDorausch@web.de>
2019-04-12 22:32:48 +0200

    Add header asset

--- my-app/src/pages/base.tsx
+++ my-app/src/pages/base.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { Header } from 'src/assets/header';
 import './base.css';
 import { Home } from './home';
 import { PickAudience } from './pick-audience';
@@ -9,13 +10,15 @@ import { UploadVideo } from './upload-video';
 class Base extends React.Component {
   public render() {
     return (
-      <Switch>
-        <Route exact={true} path='/' component={Home}/>
-        <Route path='/pick_audience' component={PickAudience}/>
-        <Route path='/upload_video' component={UploadVideo}/>
-        <Route path='/result' component={Result}/>
-      </Switch>
-      
+      <React.Fragment>
+        <Header/>
+        <Switch>
+          <Route exact={true} path='/' component={Home}/>
+          <Route path='/pick_audience' component={PickAudience}/>
+          <Route path='/upload_video' component={UploadVideo}/>
+          <Route path='/result' component={Result}/>
+        </Switch>
+      </React.Fragment>
     );
   }
 }
